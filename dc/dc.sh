abc15d5ee384aae6d4e838d9d84f1ac20698b5f2
GPF <troy_ed@yahoo.com>
2025-03-01 11:51:22 -0700

    moved game_config.txt to the /cd/gbaDC folder

--- dc/dc.sh
+++ dc/dc.sh
@@ -2,6 +2,6 @@ sh-elf-objcopy -R .stack -O binary gdC.elf gdC.bin
 rm cd/1ST_READ.BIN
 $KOS_BASE/utils/scramble/scramble gdC.bin 1ST_READ.BIN
 cp 1ST_READ.BIN cd/
-mkdcdisc  -n gbapspDC -N -f cd/gba_bios.bin -f cd/game_config.txt -d cd/gbaDC -e gdC.elf -o gbapspDC.cdi 
+mkdcdisc  -n gbapspDC -N -f cd/gba_bios.bin -d cd/gbaDC -e gdC.elf -o gbapspDC.cdi 
 
 
